fb409dcf8b8ffb2f7a17fd611a63fdf87197900c
John Skeen <jskeen@gmail.com>
2019-05-03 16:24:58 -0400

    Updates

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ document.getElementById('btn').addEventListener('click', function(){
   modal.style.display = "block";
 });
 
+document.getElementById('rules').addEventListener('click', function(){
+  modal.style.display = "block";
+});
+
 document.getElementById('close').addEventListener('click', function(){
   modal.style.display = "none";
 })
